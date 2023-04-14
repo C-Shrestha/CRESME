5b7b4b00ed67a53f62b541640c30afc8dad9e377
C-Shrestha <crason.shrestha@gmail.com>
2023-04-14 18:49:17 -0400

    delete cresme

--- CRESME/CRESME/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace CRESME.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
