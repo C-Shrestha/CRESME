88f4a8998f923d226f87935a2ee49678edde0f8f
C-Shrestha <crason.shrestha@gmail.com>
2023-04-15 16:44:45 -0400

    added Create Account button

---
+++ CRESME/Controllers/AdminController.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CRESME.Controllers
+{
+    public class AdminController : Controller
+    {
+
+
+        // Authorize makes the page availabe to only to specified Roles
+        [Authorize(Roles = "Admin, Instructor")]
+        public IActionResult GetAll()
+        {
+            return View();
+        }
+
+
+        // Authorize makes the page availabe to only to specified Roles
+        [Authorize(Roles = "Admin, Instructor")]
+        public IActionResult CreateAccounts()
+        {
+            return View();
+        }
+
+    }
+}
