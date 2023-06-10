0d25386decb16993d1c07e0d5597ea0f7c7358be
BlazingBlob <muaazz101@gmail.com>
2023-06-10 18:40:23 -0400

    Updated logic

---
+++ CRESME/Migrations/20230609224835_mergescoringdrag2.cs
@@ -0,0 +1,44 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace CRESME.Migrations
+{
+    public partial class mergescoringdrag2 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "QuizName",
+                table: "Attempts",
+                type: "nvarchar(max)",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)");
+
+            migrationBuilder.AddColumn<int>(
+                name: "QuizID",
+                table: "Attempts",
+                type: "int",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "QuizID",
+                table: "Attempts");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "QuizName",
+                table: "Attempts",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "nvarchar(max)",
+                oldNullable: true);
+        }
+    }
+}
