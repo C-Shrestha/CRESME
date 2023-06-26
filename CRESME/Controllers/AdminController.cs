001c37f8cc3a087a9478cb250a6d45eca6aea402
Nano0918 <orodriguez0918@gmail.com>
2023-06-25 22:34:51 -0400

    Added number of times images are clicked and cookies to save that number across multiple page reloads.

--- CRESME/Controllers/AdminController.cs
+++ CRESME/Controllers/AdminController.cs
@@ -9,7 +9,6 @@ using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
-using OfficeOpenXml;
 using System.Data;
 using System.Diagnostics;
 
