87f5e9cb1aec0ceee0f1405b600fadd12c53331c
C-Shrestha <crason.shrestha@gmail.com>
2023-07-05 18:01:27 -0400

    cleanup

--- CRESME/Controllers/AdminController.cs
+++ CRESME/Controllers/AdminController.cs
@@ -1363,7 +1363,6 @@ namespace CRESME.Controllers
         }
 
 
-
         //delete users of a particular block/course
         [Authorize(Roles = "Admin")]
         public IActionResult DeleteByCombinationView()
