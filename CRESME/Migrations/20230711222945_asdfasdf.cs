c3360e969be77796da9b1e1aa3ae82fc94328188
Nano0918 <orodriguez0918@gmail.com>
2023-07-11 19:27:59 -0400

    update

--- CRESME/Migrations/20230711222945_asdfasdf.cs
+++ CRESME/Migrations/20230711222945_asdfasdf.cs
@@ -5,7 +5,7 @@ using Microsoft.EntityFrameworkCore.Migrations;
 
 namespace CRESME.Migrations
 {
-    public partial class cresmeOfficial : Migration
+    public partial class asdfasdf : Migration
     {
         protected override void Up(MigrationBuilder migrationBuilder)
         {
